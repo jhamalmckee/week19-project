1c613e53a759128114592ae47c465ae130e8e143
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-17 00:26:44 -0400

    /

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = var.region
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  aws_access_key_id = "AWS_ACCESS_KEY_ID"
+  aws_secret_access_key = "AWS_SECRET_ACCESS_KEY"
   # assume_role {
   # role_arn = "arn:aws:iam::254452634027:role/EC2Creation"
   # }
