c229dca58e78c071794510b4ed907dcd29d17b43
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-18 12:27:07 -0400

    Moving environment variables

--- providers.tf
+++ providers.tf
@@ -1,9 +1,7 @@
 provider "aws" {
   region  = var.region
   # shared_credentials_file = "\\Users\\jasonceballos\\.aws\\credentials"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
-  token = "AWS_SESSION_TOKEN"
+
   # assume_role {
   # role_arn = "arn:aws:iam::254452634027:role/EC2Creation"
   # }
