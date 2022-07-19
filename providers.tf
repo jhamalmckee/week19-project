a0021f639877c57d1340a16156e67150ec8fb97d
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-19 11:31:35 -0400

    /

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ provider "aws" {
   # shared_credentials_file = "\\Users\\jasonceballos\\.aws\\credentials"
   access_key = "AWS_ACCESS_KEY_ID"
   secret_key = "AWS_SECRET_ACCESS_KEY"
-  token = "AWS_SESSION_TOKEN"
+  # token = "AWS_SESSION_TOKEN"
   # assume_role {
   # role_arn = "arn:aws:iam::254452634027:role/EC2Creation"
   # }
